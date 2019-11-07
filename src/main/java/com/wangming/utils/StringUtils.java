ef2d18632400196b6e7bc6e46d08cb3b83a75f8f
WM-0228 <2529375678@qq.com>
2019-11-07 15:07:46 +0800

    初始化版本

---
+++ src/main/java/com/wangming/utils/StringUtils.java
@@ -0,0 +1,21 @@
+/**   
+ * 
+ * @Title: StringUtils.java 
+ * @Prject: wangming-utils
+ * @Package: com.wangming.utils 
+ * @Description: TODO
+ * @author: WM  
+ * @date: 2019年11月7日 下午3:06:11 
+ * @version: V1.0   
+ */
+package com.wangming.utils;
+
+/** 
+ * @ClassName: StringUtils 
+ * @Description: TODO
+ * @author:WM 
+ * @date: 2019年11月7日 下午3:06:11  
+ */
+public class StringUtils {
+
+}
